b196db2c97cdb9a420306a696c01d9ff7df21288
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2024-10-18 19:42:59 -0700

    ifsgen/engine: update

--- ifsgen/engine.py
+++ ifsgen/engine.py
@@ -1,5 +1,30 @@
-class IFS:
-    def __init__(self) -> None:
-        pass
+import numpy as np
+from typing import Optional
+from dataclasses import dataclass
+from .types import Point
 
-    def run(self): ...
+
+# todo: include methods to visualize fractal and compute stats/access state
+
+
+@dataclass
+class Fractal: ...
+
+
+class IFS:
+    def __init__(
+        self,
+        vertices,
+        midpoint,
+        selector,
+        starting,
+        iterations,
+    ) -> None:
+        self.vertices = vertices
+        self.midpoint = midpoint
+        self.selector = selector
+
+    def run(self, iters: int, start: Optional[Point] = None) -> Fractal:
+        # this should capture DivisionByZero or OverflowErrors and still return the points graphed leading up to the exceptions
+        # there should be property or attribute on returned fractal that states whether it finished running, or the number of iters before failure
+        ...
